437c373ce3aba692e5cbd7e4a1bffdd8dbb95930
Henry Ferrara <hjferrara@bynara.com>
2019-04-23 07:33:47 -0400

    re-working to use existing broad-dsde-qa google project as uber project

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -6,8 +6,24 @@ variable "uber_project" {
   default = ""
 }
 
+variable "terra_env_owner_service_account_name" {
+  default = "terra-env-owner"
+}
+
+variable "uber_reader_service_account_name" {
+  default = "terraform-reader"
+}
+
 variable "uber_owner_service_account_name" {
   default = "terraform"
+}
+
+variable "uber_reader_service_account_iam_roles" {
+  type = "list"
+
+  default = [
+    "roles/viewer",
+  ]
 }
 
 variable "uber_owner_service_account_iam_roles" {
