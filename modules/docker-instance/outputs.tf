3efe8bc68c9b666c0fe9759f7d3f13e4cfb915f1
Raphael Luckom <rluckom@broadinstitute.org>
2019-06-13 14:06:04 -0400

    remove modules

--- modules/docker-instance/outputs.tf
+++
@@ -1,9 +0,0 @@
-
-output "instance_public_ips" {
-  value = "${google_compute_address.instance-public-ip.*.address}"
-}
-
-output "instance_instance_group" {
-  value = "${google_compute_instance_group.instance-group-unmanaged.*.self_link}"
-}
-
--- modules/http-load-balancer/outputs.tf
+++
@@ -1,5 +0,0 @@
-
-output "load_balancer_public_ip" {
-  value = "${google_compute_global_address.load-balancer-pub-ip.*.address}"
-}
-
