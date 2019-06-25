85ad68d7ea556f9342ec95ed7731bbeb9f34f834
Raphael Luckom <rluckom@broadinstitute.org>
2019-06-25 12:38:22 -0400

    cleanup

--- profiles/thurloe/terraform/thurloe/vault.tf
+++ profiles/thurloe/terraform/thurloe/vault.tf
@@ -1,6 +1,5 @@
 provider vault {}
 
-# this token will get saved in the state file, which is why a low-scope, time-limited token (and not the user's token) is appropriate.
 resource "vault_generic_secret" "database-credentials" {
   path = "secret/dsde/firecloud/ephemeral/${var.environment}/thurloe/secrets/app_sql_user"
 
