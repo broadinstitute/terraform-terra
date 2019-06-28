96d7dd4cdbb930419f9e93dec8995ceb5875eecc
Raphael Luckom <rluckom@broadinstitute.org>
2019-06-28 01:11:08 -0400

    save SA key for Vault SA

--- profiles/thurloe-sa/terraform/thurloe/sa.tf
+++ profiles/thurloe-sa/terraform/thurloe/sa.tf
@@ -15,3 +15,14 @@ resource "google_storage_bucket_iam_member" "app_config" {
   role   = "roles/storage.objectViewer"
   member = "serviceAccount:${google_service_account.app_config.email}"
 }
+
+resource "google_service_account_key" "app_account_key" {
+  service_account_id = "${google_service_account.app_config.name}"
+}
+
+provider "vault" {}
+
+resource "vault_generic_secret" "app_account_key" {
+  path = "${var.vault_path_prefix}/${var.service}/${var.service}-account.json"
+  data_json = "${base64decode(google_service_account_key.app_account_key.private_key)}"
+}
