e110305bcde9fb6333ee2ce0ea430d4ebcb3abc7
Raphael Luckom <rluckom@broadinstitute.org>
2019-07-03 00:41:26 -0400

    convert sam

--- profiles/sam/terraform/sam/sam.tf
+++ profiles/sam/terraform/sam/sam.tf
@@ -1,87 +1,29 @@
+provider "google" {
+  alias = "dns"
+  project     = "${var.dns_project}"
+  region      = "${var.dns_region}"
+  credentials = "${file("dns_sa.json")}"
+}
 
-# Docker instance(s)
-module "instances" {
-  source        = "github.com/broadinstitute/terraform-shared.git//terraform-modules/docker-instance?ref=docker-instance-0.1.1"
-
+module "load-balanced-instances" {
+  source        = "github.com/broadinstitute/terraform-shared.git//terraform-modules/load-balanced-instances?ref=rl-load-balanced-instances"
   providers {
-    google.target =  "google"
+    google.instances =  "google"
+    google.dns =  "google.dns"
   }
-  project       = "${var.google_project}"
-  instance_name = "${var.service}"
+  google_project = "${var.google_project}"
+  dns_zone = "${var.dns_zone_name}"
+  owner = "${var.owner}"
+  service = "${var.service}"
+  vault_path_prefix = "${var.vault_path_prefix}"
+  dns_project = "${var.dns_project}"
+  dns_region = "${var.dns_region}"
+  google_compute_ssl_certificate_black = "${var.google_compute_ssl_certificate_black}"
+  google_compute_ssl_certificate_red = "${var.google_compute_ssl_certificate_red}"
+  google_network_name = "${var.google_network_name}"
+  config_reader_service_account = "${var.config_reader_service_account}"
+  instance_tags = "${var.instance_tags}"
   instance_num_hosts = "${var.instance_num_hosts}"
   instance_size = "${var.instance_size}"
-  instance_service_account = "${data.google_service_account.config_reader.email}"
-  instance_network_name = "${data.google_compute_network.terra-env-network.name}"
-  instance_labels = {
-    "app" = "${var.service}",
-    "owner" = "${var.owner}",
-    "ansible_branch" = "master",
-    "ansible_project" = "terra-env",
-  }
-  instance_tags = "${var.instance_tags}"
-}
-
-# Service config bucket
-resource "google_storage_bucket" "config-bucket" {
-  name       = "${var.owner}-${var.service}-config"
-  project    = "${var.google_project}"
-  versioning = {
-    enabled = "true"
-  }
-  # Do we want to add encryption to this bucket?
-  force_destroy = true
-  labels = {
-    "app" = "${var.service}",
-    "owner" = "${var.owner}",
-    "role" = "config"
-  }
-}
-
-# Grant service account access to the config bucket
-resource "google_storage_bucket_iam_member" "app_config" {
-  bucket = "${google_storage_bucket.config-bucket.name}"
-  role   = "${element(var.storage_bucket_roles, count.index)}"
-  member = "serviceAccount:${data.google_service_account.config_reader.email}"
-}
-
-# Instance DNS
-resource "google_dns_record_set" "instance-dns" {
-  provider     = "google"
-  count        = "${var.instance_num_hosts}"
-  managed_zone = "${data.google_dns_managed_zone.terra-env-dns-zone.name}"
-  name         = "${format("${var.service}-%02d.%s",count.index+1,data.google_dns_managed_zone.terra-env-dns-zone.dns_name)}"
-  type         = "A"
-  ttl          = "${var.dns_ttl}"
-  rrdatas      = [ "${element(module.instances.instance_public_ips, count.index)}" ]
-  depends_on   = ["module.instances", "data.google_dns_managed_zone.terra-env-dns-zone"]
-}
-
-# Load Balancer
-#  need to figure out dependency in order to ensure proper order - instances 
-#  must be created before load balancer
-#  Potential solution: https://github.com/hashicorp/terraform/issues/1178#issuecomment-207369534
-module "load-balancer" {
-  source        = "github.com/broadinstitute/terraform-shared.git//terraform-modules/http-load-balancer?ref=http-load-balancer-0.1.0"
-
-  providers {
-    google.target =  "google"
-  }
-  project       = "${var.google_project}"
-  load_balancer_name = "${var.owner}-${var.service}"
-  load_balancer_ssl_certificates = [
-    "${data.google_compute_ssl_certificate.terra-env-wildcard-ssl-certificate-red.name}",
-    "${data.google_compute_ssl_certificate.terra-env-wildcard-ssl-certificate-black.name}"
-  ]
-  load_balancer_instance_groups = "${element(module.instances.instance_instance_group,0)}"
-}
-
-# Service DNS
-resource "google_dns_record_set" "app-dns" {
-  provider     = "google"
-  managed_zone = "${data.google_dns_managed_zone.terra-env-dns-zone.name}"
-  name         = "${var.service}.${data.google_dns_managed_zone.terra-env-dns-zone.dns_name}"
-  type         = "A"
-  ttl          = "${var.dns_ttl}"
-  rrdatas      = [ "${module.load-balancer.load_balancer_public_ip}" ]
-  depends_on   = ["module.load-balancer", "data.google_dns_managed_zone.terra-env-dns-zone"]
+  storage_bucket_roles = "${var.storage_bucket_roles}"
 }
