9d7a179af5570e62c81040547285efbdafee2b3a
Greg Malkov <gmalkov@broadinstitute.org>
2019-07-17 17:51:32 -0400

    add google project number

--- profiles/populate-vault/terraform/populate-vault/common.tf
+++ profiles/populate-vault/terraform/populate-vault/common.tf
@@ -37,3 +37,17 @@ resource "vault_generic_secret" "proxy_ldap" {
   path = "${var.vault_path_prefix}/common/proxy-ldap"
   data_json = "${var.proxy_ldap}"
 }
+
+data "google_project" "google_project" {
+  project_id = "${var.google_project}"
+}
+
+resource "vault_generic_secret" "google_project_id" {
+  path = "${var.vault_path_prefix}/common/google_project_number"
+  depends_on = [ "data.google_project.google_project" ]
+  data_json = <<EOT
+{
+  "number": "${data.google_project.google_project.number}"
+}
+EOT
+}
