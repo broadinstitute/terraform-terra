24b9ead2b0633cff70d2528c9dbd5f6423ea2bed
Greg Malkov <gmalkov@broadinstitute.org>
2019-07-17 16:49:30 -0400

    add manual step warning

--- profiles/sam-sa/terraform/sam/outputs.tf
+++ profiles/sam-sa/terraform/sam/outputs.tf
@@ -1,3 +1,7 @@
+output "ATTENTION!!!" {
+  value = "\nTHIS PROFILE REQUIRES MANUAL STEPS!\nSee manual-steps.md in the profile folder!\n\n"
+}
+
 output "directory_sa_ids" {
   value = "${google_service_account.directory_sa_group.*.unique_id}"
 }
