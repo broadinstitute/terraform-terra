91409900844e580533693657a8a1d0940fbe0918
Raphael Luckom <rluckom@broadinstitute.org>
2019-07-19 15:19:55 -0400

    output sam app sa as well

--- profiles/sam-sa/terraform/sam/outputs.tf
+++ profiles/sam-sa/terraform/sam/outputs.tf
@@ -5,3 +5,7 @@ output "ATTENTION!!!" {
 output "directory_sa_ids" {
   value = "${google_service_account.directory_sa_group.*.unique_id}"
 }
+
+output "sam_sa_ids" {
+  value = "${google_service_account.app.unique_id}"
+}
