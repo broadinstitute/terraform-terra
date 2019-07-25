67c86be49c2e084e31d01990c027b5270ec6f1af
Raphael Luckom <rluckom@broadinstitute.org>
2019-07-25 12:25:41 -0400

    reader first draft

---
+++ profiles/cromwell-reader/terraform/cromwell-reader/load-balanced-instances.tf
@@ -0,0 +1,28 @@
+data "google_client_config" "dns" {
+  provider = "google.dns"
+}
+module "load-balanced-instances" {
+  source        = "github.com/broadinstitute/terraform-shared.git//terraform-modules/load-balanced-instances?ref=rl-lbi-bucket-switch"
+  providers {
+    google.instances =  "google"
+    google.dns =  "google.dns"
+  }
+  instance_project = "${var.google_project}"
+  dns_zone_name = "${var.dns_zone_name}"
+  config_bucket_enable = 0
+  app_name_override = "cromwell"
+  owner = "${var.owner}"
+  service = "${var.service}"
+  load_balancer_health_check_path = "/engine/v1/status"
+  dns_project = "${data.google_client_config.dns.project}"
+  ssl_policy_name = "${var.default_ssl_policy}"
+  google_compute_ssl_certificate_black = "${var.google_compute_ssl_certificate_black}"
+  google_compute_ssl_certificate_red = "${var.google_compute_ssl_certificate_red}"
+  google_network_name = "${var.google_network_name}"
+  config_reader_service_account = "${var.config_reader_service_account}"
+  instance_tags = "${var.instance_tags}"
+  instance_num_hosts = "${var.instance_num_hosts}"
+  instance_size = "${var.instance_size}"
+  storage_bucket_roles = "${var.storage_bucket_roles}"
+  ansible_branch = "rl-add-services"
+}
--- profiles/cromwell-sa/terraform/cromwell/sa.tf
+++ profiles/cromwell-sa/terraform/cromwell/sa.tf
@@ -21,8 +21,6 @@ resource "google_service_account_key" "app_account_key" {
   service_account_id = "${google_service_account.app.name}"
 }
 
-provider "vault" {}
-
 resource "vault_generic_secret" "app_account_key" {
   path = "${var.vault_path_prefix}/${var.service}/${var.service}-account.json"
   data_json = "${base64decode(google_service_account_key.app_account_key.private_key)}"
