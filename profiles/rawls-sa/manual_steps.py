5d2a0f1bd4f01b0c84bc676bbb9ed7e07bcee662
Greg Malkov <gmalkov@broadinstitute.org>
2019-07-29 15:54:01 -0400

    update rawls manual steps

--- profiles/rawls-sa/manual_steps.py
+++ profiles/rawls-sa/manual_steps.py
@@ -4,9 +4,8 @@ def wait_for_enter():
     raw_input("Press Enter to continue: ")
  
 class AddSAScopes(object):
-    admin_console_url = "https://admin.google.com"
     def run(self, context):
-        print("\nIn the GSuite admin console ({0}) for 'test.firecloud.org', go to:".format(self.admin_console_url))
+        print("\nIn the GSuite admin console (https://admin.google.com) for 'test.firecloud.org', go to:")
         print(" Security -> Advanced Settings -> Manage API client access")
         print("For each numeric {0} SA ID that was output when this profile was run, add these scopes:".format(context["app"]))
         print("https://www.googleapis.com/auth/admin.directory.group,https://www.googleapis.com/auth/admin.directory.user\n")
@@ -18,6 +17,13 @@ class DomainWideDelegation(object):
         print("  https://console.cloud.google.com/iam-admin/serviceaccounts")
         print("Find the {0} service account (search for '{0}')".format(context["app"]))
         print("Enable domain-wide delegation for that account\n")
+        wait_for_enter()
+
+class AuthorizeDomain(object):
+    def run(self, context):
+        print("\nIn the GCP console go to APIs & Services > Credentials > OAuth consent screen:")
+        print("  https://console.cloud.google.com/apis/credentials/consent")
+        print("Add {0} to the list of authorized domains\n".format(context["dns_domain"]))
         wait_for_enter()
 
 class Oauth(object):
@@ -50,20 +56,61 @@ class Oauth(object):
         print("  https://console.cloud.google.com/apis/credentials")
         print("Create a new 'OAuth client ID' credential, selecting 'Web application' for the type")
         print("  Add the following authorized Javascript origins:\n" + origins)
-        print("  Add the following authorized redirect URIs:\n" + redirects)
+        print("  Add the following authorized redirect URIs:\n{0}\n".format(redirects))
         wait_for_enter()
+        print("\nDownload the OAuth credential JSON")
+        print("Upload the JSON to vault:")
+        print((
+            "  docker run --rm -it "
+            "-v ${{HOME}}/.vault-token:/root/.vault-token "
+            "-v ~/Downloads:/downloads broadinstitute/dsde-toolbox "
+            "vault write secret/dsde/firecloud/ephemeral/{0}/{1}/{1}-oauth-credential.json "
+            "/downloads/[JSON file name]\n"
+            ).format(
+                context["project_name"],
+                context["app"]
+            )
+        )
+        wait_for_enter()
+
+class CreateGroups(object):
+    def run(self, context):
+        print("\nIn the GSuite admin console (https://admin.google.com) for 'test.firecloud.org', go to:")
+        print(" Groups -> Create group -> Create 2 Groups:")
+        print("Admins:")
+        print("  Name: fc-admins-{0}".format(context["project_name"]))
+        print("  Email: fc-ADMINS@{0}.{1}".format(
+                context["project_name"],
+                context["google_app_domain"]
+            )
+        )
+        print("  Access type: Restricted")
+        wait_for_enter()
+        print("Curators:")
+        print("  Name: fc-curators-{0}".format(context["project_name"]))
+        print("  Email: fc-CURATORS@{0}.{1}".format(
+                context["project_name"],
+                context["google_app_domain"]
+            )
+        )
+        print("  Access type: Restricted\n")
+        wait_for_enter()
+
 
 if __name__ == "__main__":
     context = {
         "app": "rawls",
         "dns_domain": "dsde-perf.broadinstitute.org",
+        "google_app_domain": "ephemeral.test.firecloud.org",
         "project_name": sys.argv[1]
     }
     procedure = [
         AddSAScopes(),
         DomainWideDelegation(),
-        Oauth()
+        AuthorizeDomain(),
+        Oauth(),
+        CreateGroups()
     ]
     for step in procedure:
         step.run(context)
-    print("Done.")
+    print("\nAll Done!")
