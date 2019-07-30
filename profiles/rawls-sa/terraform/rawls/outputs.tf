3ad563c32edb98fc685411d778da8c5f69a864fc
Greg Malkov <gmalkov@broadinstitute.org>
2019-07-30 13:11:22 -0400

    add outputs

--- profiles/rawls-sa/terraform/rawls/outputs.tf
+++ profiles/rawls-sa/terraform/rawls/outputs.tf
@@ -2,6 +2,31 @@ output "ATTENTION!!!" {
   value = "\nTHIS PROFILE REQUIRES MANUAL STEPS!\nSee manual_steps.py in the profile folder!\n\n"
 }
 
+data "null_data_source" "dns_zone_no_trailing_dot" {
+  inputs = {
+    zone = "${substr(data.google_dns_managed_zone.dns-zone.dns_name, 0, length(data.google_dns_managed_zone.dns-zone.dns_name) - 1)}"
+  }
+}
+
 output "rawls_sa_id" {
   value = "${google_service_account.app.unique_id}"
+}
+
+output "authorized_javascript_origins" {
+  value = [
+    "https://${var.owner}-${var.service}.${data.null_data_source.dns_zone_no_trailing_dot.outputs.zone}",
+    "https://${var.owner}-firecloud.${data.null_data_source.dns_zone_no_trailing_dot.outputs.zone}"
+  ]
+}
+
+output "authorized_redirect_urls" {
+  value = [
+    "https://${var.owner}-${var.service}/oauth2callback",
+    "https://${var.owner}-${var.service}/o2c.html",
+    "https://${var.owner}-firecloud.${data.null_data_source.dns_zone_no_trailing_dot.outputs.zone}/oauth2callback",
+    "https://${var.owner}-firecloud.${data.null_data_source.dns_zone_no_trailing_dot.outputs.zone}/o2c.html",
+    "https://${var.owner}-firecloud-orchestration.${data.null_data_source.dns_zone_no_trailing_dot.outputs.zone}/o2c.html",
+    "https://${var.owner}-sam.${data.null_data_source.dns_zone_no_trailing_dot.outputs.zone}/o2c.html",
+    "https://${var.owner}-thurloe.${data.null_data_source.dns_zone_no_trailing_dot.outputs.zone}/o2c.html"
+  ]
 }
