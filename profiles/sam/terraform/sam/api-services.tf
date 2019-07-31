820f663deaa39645484f4aa9113b19bc38d50e5f
Raphael Luckom <rluckom@broadinstitute.org>
2019-07-31 14:22:22 -0400

    update api-services to tf0.12-compat

--- profiles/sam/terraform/sam/api-services.tf
+++ profiles/sam/terraform/sam/api-services.tf
@@ -1,5 +1,5 @@
 module "enable-services" {
-  source      = "github.com/broadinstitute/terraform-shared.git//terraform-modules/api-services?ref=services-0.1.2"
+  source      = "github.com/broadinstitute/terraform-shared.git//terraform-modules/api-services?ref=services-0.2.0-tf-0.12"
   enable_flag = "1"
   providers {
     google.target = "google"
