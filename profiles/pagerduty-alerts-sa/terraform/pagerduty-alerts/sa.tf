ce890ee18df72bef4bcfe6ca6f88a78edd0eddd1
Raphael Luckom <rluckom@broadinstitute.org>
2019-08-07 12:04:06 -0400

    pagerduty alerts stuff

---
+++ profiles/pagerduty-alerts-sa/terraform/pagerduty-alerts/sa.tf
@@ -0,0 +1,11 @@
+resource "google_service_account" "app" {
+  account_id   = "${var.service}"
+  project      = "${var.google_project}"
+}
+
+resource "google_project_iam_member" "app" {
+  count   = "${length(var.app_sa_roles)}"
+  project = "${var.google_project}"
+  role    = "${element(var.app_sa_roles, count.index)}"
+  member  = "serviceAccount:${google_service_account.app.email}"
+}
---
+++ profiles/pagerduty-alerts/terraform/pagerduty-alerts/service.tf
@@ -0,0 +1,48 @@
+# Docker instance(s)
+module "instances" {
+  source        = "github.com/broadinstitute/terraform-shared.git//terraform-modules/docker-instance-data-disk?ref=docker-instance-data-disk-0.1.3"
+
+  providers {
+    google.target =  "google"
+  }
+  project       = "${var.google_project}"
+  instance_name = "${var.service}"
+  instance_num_hosts = "${var.instance_num_hosts}"
+  instance_size = "${var.instance_size}"
+  instance_data_disk_size = "${var.instance_data_disk_size}"
+  instance_data_disk_type = "${var.instance_data_disk_type}"
+  instance_data_disk_name = "${var.service}-data-disk"
+  instance_service_account = "${data.google_service_account.config_reader.email}"
+  instance_network_name = "${data.google_compute_network.terra-env-network.name}"
+  instance_labels = {
+    "app" = "${var.service}",
+    "owner" = "${var.owner}",
+    "role" = "frontend",
+    "ansible_branch" = "hackathon-pagerduty-alerts",
+    "ansible_project" = "terra-env",
+  }
+  instance_tags = "${var.instance_tags}"
+}
+
+# Service config bucket
+resource "google_storage_bucket" "config-bucket" {
+  name       = "${var.owner}-${var.service}-config"
+  project    = "${var.google_project}"
+  versioning = {
+    enabled = "true"
+  }
+  force_destroy = true
+  labels = {
+    "app" = "${var.service}",
+    "owner" = "${var.owner}",
+    "role" = "config"
+  }
+}
+
+# Grant service account access to the config bucket
+resource "google_storage_bucket_iam_member" "app_config" {
+  count = "${length(var.storage_bucket_roles)}"
+  bucket = "${google_storage_bucket.config-bucket.name}"
+  role   = "${element(var.storage_bucket_roles, count.index)}"
+  member = "serviceAccount:${data.google_service_account.config_reader.email}"
+}
