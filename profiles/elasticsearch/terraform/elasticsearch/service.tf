a28198672dd16cab22f0d6416c30e19fd97d8de7
Raphael Luckom <rluckom@broadinstitute.org>
2019-08-21 11:36:10 -0400

    es use config branch

--- profiles/elasticsearch/terraform/elasticsearch/service.tf
+++ profiles/elasticsearch/terraform/elasticsearch/service.tf
@@ -18,7 +18,7 @@ module "instances" {
     "app" = "${var.service}",
     "owner" = "${var.owner}",
     "role" = "frontend",
-    "ansible_branch" = "master",
+    "ansible_branch" = "rl-set-fs-permissions",
     "ansible_project" = "terra-env",
   }
   instance_tags = "${var.instance_tags}"
