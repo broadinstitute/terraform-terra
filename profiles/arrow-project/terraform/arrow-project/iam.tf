a1d7f3252aaa522e631f2d7c229af2e1151f844c
Rob Meffan <rmeffan@users.noreply.github.com>
2019-08-28 15:06:08 -0400

    Add IAM to arrow-project; add terra-arrow-alpha values.

---
+++ profiles/arrow-project/terraform/arrow-project/iam.tf
@@ -0,0 +1,14 @@
+resource "google_project_iam_member" "project-editors" {
+  count   = "${length(var.project_editors)}"
+  project = "${var.google_project_id}"
+  role    = "roles/editor"
+  member  = "${var.project_editors[count.index]}"
+}
+
+resource "google_project_iam_member" "project-owners" {
+  count   = "${length(var.project_owners)}"
+  project = "${var.google_project_id}"
+  role    = "roles/owner"
+  member  = "${var.project_owners[count.index]}"
+}
+
