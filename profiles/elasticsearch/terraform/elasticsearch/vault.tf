2bc18dcbd65cdaa78c12f572c90eb5be04d943e4
Raphael Luckom <rluckom@broadinstitute.org>
2019-08-28 16:43:22 -0400

    update es secrets

--- profiles/elasticsearch/terraform/elasticsearch/vault.tf
+++ profiles/elasticsearch/terraform/elasticsearch/vault.tf
@@ -1,5 +1,17 @@
 provider vault {}
 
+resource "vault_generic_secret" "service-hostname" {
+  path = "${var.vault_path_prefix}/${var.service}/secrets/hostname"
+
+  data_json = <<EOT
+{
+  "hostname": "${length(module.elasticsearch.instance_hostnames) > 0 ? element(module.elasticsearch.instance_hostnames, 0) : ""}",
+  "priv_hostname": "${length(module.elasticsearch.instance_hostnames) > 0 ? element(module.elasticsearch.instance_priv_hostnames, 0) : ""}"
+}
+EOT
+  depends_on = [module.elasticsearch]
+}
+
 resource "vault_generic_secret" "hostnames" {
   path = "${var.vault_path_prefix}/${var.service}/secrets/hostnames"
   data_json = <<EOT
