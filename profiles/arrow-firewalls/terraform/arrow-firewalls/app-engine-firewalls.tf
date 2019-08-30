203666862835d3fcf246dc39411e267e9faf6c23
Brian Reilly <breilly@broadinstitute.org>
2019-08-30 11:34:05 -0400

    added staging and prod firewall rules (with rluckom) fixed tf profile to make rule priorities unique

--- profiles/arrow-firewalls/terraform/arrow-firewalls/app-engine-firewalls.tf
+++ profiles/arrow-firewalls/terraform/arrow-firewalls/app-engine-firewalls.tf
@@ -8,7 +8,7 @@ resource "google_app_engine_firewall_rule" "default-deny" {
 resource "google_app_engine_firewall_rule" "allow-orch" {
   count = "${length(var.allowed_source_ips)}"
   project = "${var.google_project}"
-  priority = 1000
+  priority = 1000 + count.index
   action = "ALLOW"
   source_range = "${var.allowed_source_ips[count.index]}"
 }
