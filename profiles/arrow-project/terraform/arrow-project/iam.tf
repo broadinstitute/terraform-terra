0b30bbefabb4c65e2a8d25f9030722d69d5dc30c
Raphael Luckom <rluckom@broadinstitute.org>
2019-08-30 18:42:38 -0400

    add sas and SA keys, move state to secure bucket

--- profiles/arrow-project/terraform/arrow-project/iam.tf
+++ profiles/arrow-project/terraform/arrow-project/iam.tf
@@ -11,4 +11,24 @@ resource "google_project_iam_member" "project-owners" {
   role    = "roles/owner"
   member  = "${var.project_owners[count.index]}"
 }
+# Application service account
+resource "google_service_account" "terraform" {
+  account_id   = "${var.google_project}-terraform"
+  project      = "${var.google_project}"
+  display_name = "${var.google_project}-terraform"
+}
 
+resource "google_project_iam_member" "terraform-account" {
+  project = "${var.google_project}"
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.terraform.email}"
+}
+
+resource "google_service_account_key" "terraform_account_key" {
+  service_account_id = "${google_service_account.terraform.name}"
+}
+
+resource "vault_generic_secret" "terraform_account_key" {
+  path = "${var.terraform_sa_key_path}"
+  data_json = "${base64decode(google_service_account_key.terraform_account_key.private_key)}"
+}
