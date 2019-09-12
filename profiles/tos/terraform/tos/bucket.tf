ef53c132b46583975ba6760d8ef9a6998996d33a
Raphael Luckom <rluckom@broadinstitute.org>
2019-09-12 15:28:31 -0400

    update bucket location

--- profiles/tos/terraform/tos/bucket.tf
+++ profiles/tos/terraform/tos/bucket.tf
@@ -1,6 +1,7 @@
 # Create the token / function storage bucket
 resource "google_storage_bucket" "function-deploy" {
   name          = "${var.owner}-${var.environment}-${var.service}-function-deploy"
+  project       = "${var.google_project}"
   force_destroy = true
   storage_class = "MULTI_REGIONAL"
 }
