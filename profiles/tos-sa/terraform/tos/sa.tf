9fe631a69dc00a200a84dc8fad289506baab90e0
Greg Malkov <gmalkov@broadinstitute.org>
2019-09-13 15:40:31 -0400

    add tos permissions to orch SA

--- profiles/tos-sa/terraform/tos/sa.tf
+++ profiles/tos-sa/terraform/tos/sa.tf
@@ -9,3 +9,15 @@ resource "google_project_iam_member" "app" {
   role    = "${element(var.app_sa_roles, count.index)}"
   member  = "serviceAccount:${google_service_account.app_config.email}"
 }
+
+data "google_service_account" "orch_sa" {
+  account_id  = "firecloud-orchestration"
+  project = "${var.main_env_project}"
+}
+
+resource "google_project_iam_member" "orch" {
+  count   = "${length(var.orch_sa_roles)}"
+  project = "${var.google_project}"
+  role    = "${element(var.orch_sa_roles, count.index)}"
+  member  = "serviceAccount:${data.google_service_account.orch_sa.email}"
+}
