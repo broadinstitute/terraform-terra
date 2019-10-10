264317e725eb4eae3207ed3dade01b81fdb305fb
Greg Malkov <gmalkov@broadinstitute.org>
2019-10-10 16:45:28 -0400

    naming

--- profiles/terra-ui/terraform/terra-ui/vault.tf
+++ profiles/terra-ui/terraform/terra-ui/vault.tf
@@ -1,9 +1,9 @@
-resource "vault_generic_secret" "endpoint" {
-  path = "${var.vault_path_prefix}/${var.service}/secrets/endpoint"
+resource "vault_generic_secret" "url" {
+  path = "${var.vault_path_prefix}/${var.service}/secrets/url"
 
   data_json = <<EOT
 {
-  "endpoint": "${var.owner}-${var.environment}-${var.service}-dot-${data.google_client_config.app-engine.project}.appspot.com"
+  "url": "${var.owner}-${var.environment}-${var.service}-dot-${data.google_client_config.app-engine.project}.appspot.com"
 }
 EOT
 }
