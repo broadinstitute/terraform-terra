04812e4f6fec7e34775ace0749072312b99dca74
Raphael Luckom <rluckom@broadinstitute.org>
2019-10-16 15:55:38 -0400

    fcui secret updates

--- profiles/populate-vault/terraform/populate-vault/common.tf
+++ profiles/populate-vault/terraform/populate-vault/common.tf
@@ -13,7 +13,7 @@ EOT
 }
 
 resource "vault_generic_secret" "bond_dummy_url" {
-  path = "${var.vault_path_prefix}/bond/secret/url"
+  path = "${var.vault_path_prefix}/bond/secrets/url"
   data_json = <<EOT
 {
   "url": "https://${var.owner}-bond.appspot.com"
@@ -22,7 +22,7 @@ EOT
 }
 
 resource "vault_generic_secret" "martha_dummy_urls" {
-  path = "${var.vault_path_prefix}/martha/secret/urls"
+  path = "${var.vault_path_prefix}/martha/secrets/urls"
   data_json = <<EOT
 {
   "fileSummary": "https://us-central1-${var.google_project}.cloudfunctions.net/fileSummaryV1",
@@ -32,7 +32,7 @@ EOT
 }
 
 resource "vault_generic_secret" "shibboleth_dummy_url" {
-  path = "${var.vault_path_prefix}/shibboleth/secret/url"
+  path = "${var.vault_path_prefix}/shibboleth/secrets/url"
   data_json = <<EOT
 {
   "url": "http://mock-nih.dev.test.firecloud.org"
