b11dcf1f65fece4430662d87ef7d089e18fe0571
Raphael Luckom <rluckom@broadinstitute.org>
2019-10-21 13:18:07 -0400

    use tag

--- profiles/oauth-ids/terraform/oauth-ids/vault.tf
+++ profiles/oauth-ids/terraform/oauth-ids/vault.tf
@@ -24,49 +24,49 @@ data "null_data_source" "env_specific_aou_oauth_audiences" {
 }
 
 module "legacy_oauth_audiences" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=rl-merged-oauth-audiences"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=merged_oauth_audiences-0.0.0-tf-0.12"
   original_audience_secret_path = "secret/dsde/firecloud/common/oauth_client_id"
   new_audience_secret_path = "${var.vault_path_prefix}/common/oauth_client_id"
   values_to_merge = data.null_data_source.env_specific_oauth_audiences.outputs
 }
 
 module "terra_oauth_audiences" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=rl-merged-oauth-audiences"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=merged_oauth_audiences-0.0.0-tf-0.12"
   original_audience_secret_path = "secret/dsde/firecloud/common/non_aou_oauth_client_ids"
   new_audience_secret_path = "${var.vault_path_prefix}/common/non_aou_oauth_client_ids"
   values_to_merge = data.null_data_source.env_specific_oauth_audiences.outputs
 }
 
 module "perf_fake_research_aou_oauth_client_ids" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=rl-merged-oauth-audiences"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=merged_oauth_audiences-0.0.0-tf-0.12"
   original_audience_secret_path = "secret/dsde/firecloud/common/perf_fake_research_aou_oauth_client_ids"
   new_audience_secret_path = "${var.vault_path_prefix}/common/perf_fake_research_aou_oauth_client_ids"
   values_to_merge = data.null_data_source.env_specific_aou_oauth_audiences.outputs
 }
 
 module "research_aou_oauth_client_ids" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=rl-merged-oauth-audiences"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=merged_oauth_audiences-0.0.0-tf-0.12"
   original_audience_secret_path = "secret/dsde/firecloud/common/research_aou_oauth_client_ids"
   new_audience_secret_path = "${var.vault_path_prefix}/common/research_aou_oauth_client_ids"
   values_to_merge = data.null_data_source.env_specific_aou_oauth_audiences.outputs
 }
 
 module "fake_research_aou_oauth_client_ids" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=rl-merged-oauth-audiences"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=merged_oauth_audiences-0.0.0-tf-0.12"
   original_audience_secret_path = "secret/dsde/firecloud/common/fake_research_aou_oauth_client_ids"
   new_audience_secret_path = "${var.vault_path_prefix}/common/fake_research_aou_oauth_client_ids"
   values_to_merge = data.null_data_source.env_specific_aou_oauth_audiences.outputs
 }
 
 module "stable_fake_research_aou_oauth_client_ids" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=rl-merged-oauth-audiences"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=merged_oauth_audiences-0.0.0-tf-0.12"
   original_audience_secret_path = "secret/dsde/firecloud/common/stable_fake_research_aou_oauth_client_ids"
   new_audience_secret_path = "${var.vault_path_prefix}/common/stable_fake_research_aou_oauth_client_ids"
   values_to_merge = data.null_data_source.env_specific_aou_oauth_audiences.outputs
 }
 
 module "staging_fake_research_aou_oauth_client_ids" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=rl-merged-oauth-audiences"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/merged_oauth_audiences?ref=merged_oauth_audiences-0.0.0-tf-0.12"
   original_audience_secret_path = "secret/dsde/firecloud/common/staging_fake_research_aou_oauth_client_ids"
   new_audience_secret_path = "${var.vault_path_prefix}/common/staging_fake_research_aou_oauth_client_ids"
   values_to_merge = data.null_data_source.env_specific_aou_oauth_audiences.outputs
