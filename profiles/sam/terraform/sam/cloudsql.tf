980503411404f658748697eb44731770502b5896
Raphael Luckom <rluckom@broadinstitute.org>
2019-10-22 10:20:10 -0400

    update module tag

--- profiles/sam/terraform/sam/cloudsql.tf
+++ profiles/sam/terraform/sam/cloudsql.tf
@@ -8,7 +8,7 @@ resource "random_id" "root-password" {
 
 # Cloud SQL database
 module "cloudsql" {
-  source        = "github.com/broadinstitute/terraform-shared.git//terraform-modules/cloudsql-postgres?ref=rl-postgres"
+  source        = "github.com/broadinstitute/terraform-shared.git//terraform-modules/cloudsql-postgres?ref=cloudsql-postgres-0.0.1-tf-0.12"
 
   providers = {
     google.target =  "google"
