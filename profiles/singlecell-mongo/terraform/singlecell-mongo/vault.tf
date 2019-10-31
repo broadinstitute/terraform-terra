62760e7124aa77a6ad7b6be4708c91f769ca6b6d
Eric Weitz <eweitz@broadinstitute.org>
2019-10-31 16:15:04 -0400

    Update Vault path; fix typo in JSON file

--- profiles/singlecell-mongo/terraform/singlecell-mongo/vault.tf
+++ profiles/singlecell-mongo/terraform/singlecell-mongo/vault.tf
@@ -9,7 +9,8 @@ resource "random_id" "mongodb-root-password" {
 }
 
 resource "vault_generic_secret" "app-database-credentials" {
-  path = "secret/dsde/firecloud/ephemeral/scp/${var.scp_vault_path}/mongo/user"
+  # path = "secret/kdux/scp/development/eweitz/scp_config.json
+  path = "secret/kdux/scp/${var.scp_vault_path}/mongo/user"
 
   data_json = <<EOT
 {
@@ -22,7 +23,7 @@ EOT
 resource "vault_generic_secret" "root-database-credentials" {
   # DevOps will figure out how to make this write to SCP's proper vault path
   # path = "secret/kdux/scp/${var.scp_vault_path}/mongo/root_user"
-  path = "secret/dsde/firecloud/ephemeral/scp/${var.scp_vault_path}/mongo/root_user"
+  path = "secret/kdux/scp/${var.scp_vault_path}/mongo/root_user"
 
   data_json = <<EOT
 {
@@ -33,7 +34,7 @@ EOT
 }
 
 resource "vault_generic_secret" "hostnames" {
-  path = "secret/dsde/firecloud/ephemeral/scp/${var.scp_vault_path}/mongo/hostname"
+  path = "secret/kdux/scp/${var.scp_vault_path}/mongo/hostname"
   data_json = <<EOT
 {
   "ip": ${jsonencode(module.mongodb.instance_public_ips)}
