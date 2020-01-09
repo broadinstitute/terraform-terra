afb8ed5d843067c172a8ec2621ddabf3d3173c42
Greg Malkov <gmalkov@broadinstitute.org>
2020-01-09 13:18:48 -0500

    DDO-41 create kernel k8s cluster

---
+++ profiles/terra-kernel-k8s/terraform/terra-kernel-k8s/outputs.tf
@@ -0,0 +1,7 @@
+output "ATTENTION" {
+  value = <<EOF
+THIS PROFILE REQUIRES MANUAL STEPS IF RUNNING FOR THE FIRST TIME!
+To run the manual steps run the script in the profile root:
+python manual_steps.py
+EOF
+}
