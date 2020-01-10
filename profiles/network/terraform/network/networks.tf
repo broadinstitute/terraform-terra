d432fcae1a12aad2c218d304f3a597cbb6256aef
Greg Malkov <gmalkov@broadinstitute.org>
2020-01-10 13:57:50 -0500

    not using module

--- profiles/network/terraform/network/networks.tf
+++ profiles/network/terraform/network/networks.tf
@@ -1,12 +1,8 @@
-module "network" {
-  # "github.com/" + org + "/" + repo name + ".git" + "//" + path within repo to base dir + "?ref=" + git object ref
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/logged-network?ref=logged-network-0.0.0-tf-0.12"
-  google_project     = var.google_project
-  network_name       = "${var.network_name}"
-  providers = {
-    "google-beta.target" = "google-beta"
-    "google.target" = "google"
-  }
+resource "google_compute_network" "vpc-network" {          
+  provider                = "google-beta"                                  
+  project                 = var.google_project
+  name                    = var.network_name            
+  auto_create_subnetworks = true
 }
 
 provider "google-beta" {
