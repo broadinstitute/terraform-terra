ac9f3d9dc2bcf7e71ab50d592acbe4f75a793332
Greg Malkov <gmalkov@broadinstitute.org>
2020-01-15 10:19:49 -0500

    wording

--- profiles/terra-kernel-k8s/manual_steps.py
+++ profiles/terra-kernel-k8s/manual_steps.py
@@ -5,7 +5,7 @@ def wait_for_enter():
  
 class SetupIstio(object):
     def run(self, context):
-        print("\nFollow the instructions here to install istio on the cluster:")
+        print("\nFollow the instructions here if you'd like to install istio on the cluster:")
         print("https://cloud.google.com/istio/docs/istio-on-gke/installing")
         print("Use Strict mTLS\n")
         wait_for_enter()
