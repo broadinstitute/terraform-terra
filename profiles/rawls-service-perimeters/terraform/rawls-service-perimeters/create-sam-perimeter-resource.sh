942369c38cf4dfee9f8647f77f1679714193456d
Marcus Talbott <mtalbott@broadinstitute.org>
2020-02-13 13:47:27 -0500

    some PR comments and elaborated script descriptions to be explicit about what the script does and does not do

--- profiles/rawls-service-perimeters/terraform/rawls-service-perimeters/create-sam-perimeter-resource.sh
+++ profiles/rawls-service-perimeters/terraform/rawls-service-perimeters/create-sam-perimeter-resource.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 
-# This script is run by terraform to create a service perimeter in Sam.
+# This script is run by terraform to create a service perimeter in Sam. It ensures that the caller of this script is
+# a registered user (needed to create a resource in Sam) and then it creates a service-perimeter resource in Sam. The
+# script is resilient to being called multiple times to create the same resource, but it will not update an already
+# existing service perimeter. It also cannot be used to delete the service perimeter. It is just for initial perimeter
+# setup
 
 if [[ $# -ne 4 ]]; then
     echo "usage: ./create_service-perimeter.sh ENV PERIMETER_NAME POLICY_NAME OWNER_EMAIL"
@@ -20,26 +24,31 @@ ACCESS_TOKEN=$(gcloud auth print-access-token)
 curl -X POST --header 'Content-Type: application/json' --header "Authorization: Bearer ${ACCESS_TOKEN}" \
  "${SAM_HOST}/register/user/v2/self"
 
+CREATE_PERIMETER_REQUEST_JSON=$(cat <<- EOF
+{
+  "resourceId": "accessPolicies%2F${POLICY_NAME}%2FservicePerimeters%2F${PERIMETER_NAME}",
+  "policies": {
+    "owner": {
+      "memberEmails": [
+        "${OWNER_EMAIL}"
+      ],
+      "actions": [],
+      "roles": [
+        "owner"
+      ]
+    }
+  },
+  "authDomain": []
+}
+EOF
+)
+
 # Create the perimeter in Sam, url encoded resourceId
-STATUS=$(curl -s -o /dev/null -w '%{http_code}' -X POST --header 'Content-Type: application/json' --header "Authorization: Bearer ${ACCESS_TOKEN}" -d "{ \
-   \"resourceId\": \"accessPolicies%2F${POLICY_NAME}%2FservicePerimeters%2F${PERIMETER_NAME}\", \
-   \"policies\": { \
-     \"owner\": { \
-       \"memberEmails\": [ \
-         \"${OWNER_EMAIL}\" \
-       ], \
-       \"actions\": [], \
-       \"roles\": [ \
-         \"owner\" \
-       ] \
-     } \
-   }, \
-   \"authDomain\": [] \
- }" "${SAM_HOST}/api/resources/v1/service-perimeter")
+STATUS=$(curl -s -o /dev/null -w '%{http_code}' -X POST --header 'Content-Type: application/json' --header "Authorization: Bearer ${ACCESS_TOKEN}" -d "${CREATE_PERIMETER_REQUEST_JSON}" "${SAM_HOST}/api/resources/v1/service-perimeter")
 
 # We consider the 200s as success, and also 409 if the resource already exists. If the resource already exists, this
 # script may have already run and that's ok.
-if [[ $STATUS -eq '200' ]] || [[ $STATUS -eq '204' ]] || [[ $STATUS -eq '409' ]]; then
+if [[ ${STATUS} -eq '201' ]] || [[ ${STATUS} -eq '204' ]] || [[ ${STATUS} -eq '409' ]]; then
   exit 0
 else
   exit $STATUS
