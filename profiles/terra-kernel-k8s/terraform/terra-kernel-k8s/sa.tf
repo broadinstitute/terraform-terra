f43be87a6967260eef1aa140dcff03c49727f78c
Greg Malkov <gmalkov@broadinstitute.org>
2020-02-13 10:05:43 -0500

    add CI SA

--- profiles/terra-kernel-k8s/terraform/terra-kernel-k8s/sa.tf
+++ profiles/terra-kernel-k8s/terraform/terra-kernel-k8s/sa.tf
@@ -14,3 +14,21 @@ resource "google_project_iam_member" "cloudsql" {
 resource "google_service_account_key" "cloudsql_account_key" {
   service_account_id = "${google_service_account.cloudsql.name}"
 }
+
+# CI access
+resource "google_service_account" "ci" {
+  account_id   = "${var.owner}-ci-sa"
+  project      = "${var.google_project}"
+  display_name = "${var.owner}-ci-sa"
+}
+
+resource "google_project_iam_member" "ci" {
+  count   = "${length(var.ci_sa_roles)}"
+  project = "${var.google_project}"
+  role    = "${element(var.ci_sa_roles, count.index)}"
+  member  = "serviceAccount:${google_service_account.ci.email}"
+}
+
+resource "google_service_account_key" "ci_account_key" {
+  service_account_id = "${google_service_account.ci.name}"
+}
--- profiles/terra-kernel-k8s/terraform/terra-kernel-k8s/vault.tf
+++ profiles/terra-kernel-k8s/terraform/terra-kernel-k8s/vault.tf
@@ -30,7 +30,12 @@ resource "vault_generic_secret" "database-instance-name" {
 EOT
 }
 
-resource "vault_generic_secret" "app_account_key" {
+resource "vault_generic_secret" "cloudsql_account_key" {
   path = "${var.vault_path_prefix}/${var.owner}/common/postgres/cloudsql-account.json"
   data_json = "${base64decode(google_service_account_key.cloudsql_account_key.private_key)}"
+}
+
+resource "vault_generic_secret" "ci_account_key" {
+  path = "${var.vault_path_prefix}/${var.owner}/common/ci/ci-account.json"
+  data_json = "${base64decode(google_service_account_key.ci_account_key.private_key)}"
 }
