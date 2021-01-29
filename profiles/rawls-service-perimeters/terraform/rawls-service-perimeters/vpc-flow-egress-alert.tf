3d7ca0dde94807c73cd0d49045471a1cd2b21a3b
yonghao <yyhao1@gmail.com>
2021-01-29 10:27:30 -0500

    tmp

--- profiles/rawls-service-perimeters/terraform/rawls-service-perimeters/vpc-flow-egress-alert.tf
+++ profiles/rawls-service-perimeters/terraform/rawls-service-perimeters/vpc-flow-egress-alert.tf
@@ -1,6 +1,43 @@
 # VPC flow log based egress alerting per AoU RW Service Perimeter
 # https://docs.google.com/document/d/1SiQokcrilgX12_gmgXPap2CHM-VZ3ohS4pKeLIrQGBI/edit#
 
+locals {
+  content_dir           = pathexpand("${path.module}/assets/content")
+  search_configs        = fileset(local.content_dir, "*.json")
+  content_template_path = pathexpand("${local.content_dir}/egress_window_template.json")
+  query_path            = pathexpand("${local.content_dir}/query.txt")
+
+  sumologic_egress_thresholds = {
+    alert_low_60sec_50mib = {
+      egress_threshold_mib = 50
+      egress_window_sec    = 60
+      cron_expression      = "0 * * * * ? *"
+      time_range           = "-5m"
+      schedule_type        = "RealTime"
+    }
+    alert_mid_600sec_150mib = {
+      egress_threshold_mib = 150
+      egress_window_sec    = 600
+      cron_expression      = "0 * * * * ? *"
+      time_range           = "-5m"
+      schedule_type        = "RealTime"
+    }
+    alert_high_3600sec_200mib= {
+      egress_threshold_mib = 200
+      egress_window_sec    = 3600
+      cron_expression      = "0 0 * * * ? *"
+      time_range           = "-12m"
+      schedule_type        = "1Hour"
+    }
+  }
+
+  alert_configs_and_thresholds = [
+  for pair in setproduct(var.vpc_flow_egress_alerts, local.sumologic_egress_thresholds) : {
+    egress_alert_config = pair[0]
+    egress_threshold = pair[1]
+  }
+  ]
+}
 
 # Pull Sumologic credentials from Vault
 data "vault_generic_secret" "sumologic-secret-path" {
@@ -53,7 +90,7 @@ resource "sumologic_collector" "vpc-flow-logs" {
 resource "sumologic_gcp_source" "sumologic-vpc-flow-log-source" {
   for_each      = var.perimeters
 
-  name          = replace("${each.key}-vpc-flow-log-source", "_", "-")
+  name          = concat("gcp/vpcflowlogs/aou/", lookup(lookup(vpc_flow_egress_alerts, each.key), "sumologic_source_category_name"))
   description   = "Sumologic GCP Source receives log data from Google Pub/Sub"
   category      = "gcp"
   collector_id  = sumologic_collector.vpc-flow-logs[each.key].id
@@ -82,3 +119,24 @@ resource "google_pubsub_subscription" "vpc-flow-pubsub-subscription" {
     }
   }
 }
+
+# Simply export a content file or folder and put the JSON file in ./assets/content.
+# Since the query is so long (and critical) and is json-encoded, it's easier
+# to configure it separately.
+
+resource "sumologic_content" "sumologic-vpc-flow-alert" {
+  for_each  = var.alert_configs_and_thresholds
+  parent_id = each.key.sumologic_parent_folder_id_hexadecimal
+  config =
+  config    = templatefile(local.query_path, {
+    aou_env                           = each.key.aou_env
+    sumologic_source_category_name    = lookup(each.key, "sumologic_source_category_name", 0)
+    egress_threshold_mib              = lookup(each.value, "egress_threshold_mib", 0)
+    egress_window_sec                 = lookup(each.value, "egress_window_sec", 0)
+    cron_expression      = lookup(each.value, "cron_expression", 0)
+    schedule_type        = lookup(each.value, "schedule_type", 0)
+    time_range           = lookup(each.value, "time_range", 0)
+    query_text           = lookup(local.queries_encoded, egress_rule)
+  })
+}
+
