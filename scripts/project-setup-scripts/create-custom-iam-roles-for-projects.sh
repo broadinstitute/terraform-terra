adc50701302280322d6dccd2cb7badba5a27bf5a
Andrew Jang <andy7i@users.noreply.github.com>
2021-02-18 14:00:15 -0500

    prefix "google-" to role name

--- scripts/project-setup-scripts/create-custom-iam-roles-for-projects.sh
+++ scripts/project-setup-scripts/create-custom-iam-roles-for-projects.sh
@@ -3,9 +3,9 @@
 # todo: convert this to Terraform as part of https://broadworkbench.atlassian.net/browse/CA-1195
 
 # org: test.firecloud.org
-gcloud iam roles create project-viewer --organization=400176686919 --file=project-viewer.yaml
-gcloud iam roles create project-owner --organization=400176686919 --file=project-owner.yaml
+gcloud iam roles create google-project-viewer --organization=400176686919 --file=google-project-viewer.yaml
+gcloud iam roles create google-project-owner --organization=400176686919 --file=google-project-owner.yaml
 
 # org: firecloud.org
-gcloud iam roles create project-viewer --organization=386193000800 --file=project-viewer.yaml
-gcloud iam roles create project-owner --organization=386193000800 --file=project-owner.yaml
+gcloud iam roles create google-project-viewer --organization=386193000800 --file=google-project-viewer.yaml
+gcloud iam roles create google-project-owner --organization=386193000800 --file=google-project-owner.yaml
