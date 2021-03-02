5d014b8a234f121f9d24af2df4e06b35d72d1de7
Andrew Jang <andy7i@users.noreply.github.com>
2021-03-02 18:01:53 -0500

    rename script, cleanup

--- scripts/project-setup-scripts/add-folder-roles-for-service-accounts-and-terra-billing-and-test-users.sh
+++ scripts/project-setup-scripts/add-folder-roles-for-service-accounts-and-terra-billing-and-test-users.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# set up folder access for Rawls and Cromwell SAs as editor roles, as well as billing and project-owners
+# set up folder access for Rawls and Cromwell SAs, as well as billing and project-owners
 # must have resourcemanager.projects.setIamPolicy permissions
 
 # todo: convert this to Terraform as part of https://broadworkbench.atlassian.net/browse/CA-1195
@@ -23,17 +23,17 @@ if [[ -z "${ENV}" ]]; then
   exit 1
 else
   # set FOLDER_ID and SA email based on the env
-  if [[ ${ENV} == "dev" ]] ; then
+  if [[ "${ENV}" == "dev" ]] ; then
     FOLDER_ID=599966635789 # folder: test.firecloud.org/dev
-  elif [[ ${ENV} == "qa" ]]; then
+  elif [[ "${ENV}" == "qa" ]]; then
     FOLDER_ID=950154083315 # folder: test.firecloud.org/tools (no QA folder)
-  elif [[ ${ENV} == "perf" ]]; then
+  elif [[ "${ENV}" == "perf" ]]; then
     FOLDER_ID=1076814209841 # folder: test.firecloud.org/perf
-  elif [[ ${ENV} == "alpha" ]]; then
+  elif [[ "${ENV}" == "alpha" ]]; then
     FOLDER_ID=829384679571 # folder: test.firecloud.org/alpha
-  elif [[ ${ENV} == "staging" ]]; then
+  elif [[ "${ENV}" == "staging" ]]; then
     FOLDER_ID=362889920837 # folder: test.firecloud.org/staging
-  elif [[ ${ENV} == "prod" ]]; then
+  elif [[ "${ENV}" == "prod" ]]; then
     FOLDER_ID="TBD" # folder: firecloud.org/prod # todo: to be created as part of https://broadworkbench.atlassian.net/browse/CA-1194
   else
     echo "ENV was not valid."
@@ -45,19 +45,19 @@ fi
 RAWLS_SA=$(docker run -e VAULT_TOKEN="$(cat ~/.vault-token)" -it broadinstitute/dsde-toolbox:dev vault read -field="client_email" secret/dsde/firecloud/${ENV}/rawls/rawls-account.json)
 CROMWELL_SA=$(docker run -e VAULT_TOKEN="$(cat ~/.vault-token)" -it broadinstitute/dsde-toolbox:dev vault read -field="client_email" secret/dsde/firecloud/${ENV}/cromwell/cromwell-account.json)
 
-gcloud resource-manager folders add-iam-policy-binding ${FOLDER_ID} --member=serviceAccount:${RAWLS_SA} --role=roles/editor
-gcloud resource-manager folders add-iam-policy-binding ${FOLDER_ID} --member=serviceAccount:${CROMWELL_SA} --role=roles/editor
+gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${RAWLS_SA}" --role=roles/editor
+gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${CROMWELL_SA}" --role=roles/editor
 
 # prod only needs the terra-billing group
-if [[ ${ENV} == "prod" ]]; then
-  gcloud resource-manager folders add-iam-policy-binding ${FOLDER_ID} --member=group:terra-billing@firecloud.org --role=roles/owner
+if [[ "${ENV}" == "prod" ]]; then
+  gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:terra-billing@firecloud.org --role=roles/owner
 else # for non-prod envs, add terra-billing as well as project owner groups
-  gcloud resource-manager folders add-iam-policy-binding ${FOLDER_ID} --member=group:terra-billing@test.firecloud.org --role=roles/owner
-  gcloud resource-manager folders add-iam-policy-binding ${FOLDER_ID} --member=group:firecloud-project-owners@test.firecloud.org --role=roles/owner
+  gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:terra-billing@test.firecloud.org --role=roles/owner
+  gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:firecloud-project-owners@test.firecloud.org --role=roles/owner
 
   # for the QA env, add quality.firecloud.org users as well
-  if [[ ${ENV} == "qa" ]]; then
-    gcloud resource-manager folders add-iam-policy-binding ${FOLDER_ID} --member=group:terra-billing@quality.firecloud.org --role=roles/owner
-    gcloud resource-manager folders add-iam-policy-binding ${FOLDER_ID} --member=group:firecloud-project-owners@quality.firecloud.org --role=roles/owner
+  if [[ "${ENV}" == "qa" ]]; then
+    gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:terra-billing@quality.firecloud.org --role=roles/owner
+    gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:firecloud-project-owners@quality.firecloud.org --role=roles/owner
   fi
 fi
