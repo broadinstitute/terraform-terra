5e5b9a5ec799a00fe7ca8c9383f32e3872683e9d
Andrew Jang <andy7i@users.noreply.github.com>
2021-03-04 14:41:29 -0500

    making sure we have both fiab ENVs covered

--- scripts/project-setup-scripts/add-folder-roles-for-service-accounts-and-terra-billing-and-test-users.sh
+++ scripts/project-setup-scripts/add-folder-roles-for-service-accounts-and-terra-billing-and-test-users.sh
@@ -10,7 +10,8 @@ set -euxo pipefail
 ENV=$1
 VALID_ENVS="Valid ENVs:
         dev
-        qa
+        fiab-dev
+        fiab-qa
         perf
         alpha
         staging
@@ -22,11 +23,15 @@ if [[ -z "${ENV}" ]]; then
   echo "${VALID_ENVS}"
   exit 1
 else
-  # set FOLDER_ID and SA email based on the env
+  vault_env="${ENV}"
   if [[ "${ENV}" == "dev" ]] ; then
     FOLDER_ID=599966635789 # folder: test.firecloud.org/dev
-  elif [[ "${ENV}" == "qa" ]]; then
-    FOLDER_ID=950154083315 # folder: test.firecloud.org/tools (no QA folder)
+  elif [[ "${ENV}" == "fiab-dev" ]] ; then
+    FOLDER_ID=950154083315 # folder: test.firecloud.org/tools (folder for dev FIABs)
+    vault_env=dev
+  elif [[ "${ENV}" == "fiab-qa" ]]; then
+    FOLDER_ID="TBD" # folder: quality.firecloud.org/qa # todo: to be created as part of https://broadworkbench.atlassian.net/browse/CA-1194 after completion of https://broadworkbench.atlassian.net/browse/DDO-1089
+    vault_env=qa
   elif [[ "${ENV}" == "perf" ]]; then
     FOLDER_ID=1076814209841 # folder: test.firecloud.org/perf
   elif [[ "${ENV}" == "alpha" ]]; then
@@ -34,7 +39,7 @@ else
   elif [[ "${ENV}" == "staging" ]]; then
     FOLDER_ID=362889920837 # folder: test.firecloud.org/staging
   elif [[ "${ENV}" == "prod" ]]; then
-    FOLDER_ID="TBD" # folder: firecloud.org/prod # todo: to be created as part of https://broadworkbench.atlassian.net/browse/CA-1194
+    FOLDER_ID="TBD" # folder: firecloud.org/prod # todo: to be created as part of https://broadworkbench.atlassian.net/browse/CA-1194 after completion of https://broadworkbench.atlassian.net/browse/DDO-1089
   else
     echo "ENV was not valid."
     echo "${VALID_ENVS}"
@@ -42,11 +47,11 @@ else
   fi
 fi
 
-RAWLS_SA=$(docker run -e VAULT_TOKEN="$(cat ~/.vault-token)" -it broadinstitute/dsde-toolbox:dev vault read -field="client_email" secret/dsde/firecloud/${ENV}/rawls/rawls-account.json)
-CROMWELL_SA=$(docker run -e VAULT_TOKEN="$(cat ~/.vault-token)" -it broadinstitute/dsde-toolbox:dev vault read -field="client_email" secret/dsde/firecloud/${ENV}/cromwell/cromwell-account.json)
-
+RAWLS_SA=$(docker run -e VAULT_TOKEN="$(cat ~/.vault-token)" -it broadinstitute/dsde-toolbox:dev vault read -field="client_email" secret/dsde/firecloud/${vault_env}/rawls/rawls-account.json)
+CROMWELL_SA=$(docker run -e VAULT_TOKEN="$(cat ~/.vault-token)" -it broadinstitute/dsde-toolbox:dev vault read -field="client_email" secret/dsde/firecloud/${vault_env}/cromwell/cromwell-account.json)
 gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${RAWLS_SA}" --role=roles/editor
 gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${CROMWELL_SA}" --role=roles/editor
+
 
 # prod only needs the terra-billing group
 if [[ "${ENV}" == "prod" ]]; then
@@ -56,7 +61,7 @@ else # for non-prod envs, add terra-billing as well as project owner groups
   gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:firecloud-project-owners@test.firecloud.org --role=roles/owner
 
   # for the QA env, add quality.firecloud.org users as well
-  if [[ "${ENV}" == "qa" ]]; then
+  if [[ "${ENV}" == "fiab-qa" ]]; then
     gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:terra-billing@quality.firecloud.org --role=roles/owner
     gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:firecloud-project-owners@quality.firecloud.org --role=roles/owner
   fi
