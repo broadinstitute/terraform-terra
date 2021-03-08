3d0449dfe8352b5f1dae0d42822a3ab1410e0fed
Andrew <andy7i@users.noreply.github.com>
2021-03-08 10:55:21 -0500

    [CA-1180] [CA-1179] Custom IAM roles scripts and folder roles (#144)

---
+++ scripts/project-setup-scripts/add-folder-roles-for-service-accounts-and-terra-billing-and-test-users.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+# set up folder access for Rawls and Cromwell SAs, as well as billing and project-owners
+# In order to successfully run this script, you must first `gcloud auth login` as a gcloud user with resourcemanager.projects.setIamPolicy permissions
+
+# todo: convert this to Terraform as part of https://broadworkbench.atlassian.net/browse/CA-1195
+
+set -euxo pipefail
+
+
+ENV=$1
+VALID_ENVS="Valid ENVs:
+        dev
+        fiab-dev
+        fiab-qa
+        perf
+        alpha
+        staging
+        prod
+       "
+
+if [[ -z "${ENV}" ]]; then
+  echo "Usage: ./add-folder-roles-to-service-accounts.sh <ENV>"
+  echo "${VALID_ENVS}"
+  exit 1
+else
+  vault_env="${ENV}"
+  if [[ "${ENV}" == "dev" ]] ; then
+    FOLDER_ID=599966635789 # folder: test.firecloud.org/dev
+  elif [[ "${ENV}" == "fiab-dev" ]] ; then
+    FOLDER_ID=950154083315 # folder: test.firecloud.org/tools (folder for dev FIABs)
+    vault_env=dev
+  elif [[ "${ENV}" == "fiab-qa" ]]; then
+    FOLDER_ID="TBD" # folder: quality.firecloud.org/qa # todo: to be created as part of https://broadworkbench.atlassian.net/browse/CA-1194 after completion of https://broadworkbench.atlassian.net/browse/DDO-1089
+    vault_env=qa
+  elif [[ "${ENV}" == "perf" ]]; then
+    FOLDER_ID=1076814209841 # folder: test.firecloud.org/perf
+  elif [[ "${ENV}" == "alpha" ]]; then
+    FOLDER_ID=829384679571 # folder: test.firecloud.org/alpha
+  elif [[ "${ENV}" == "staging" ]]; then
+    FOLDER_ID=362889920837 # folder: test.firecloud.org/staging
+  elif [[ "${ENV}" == "prod" ]]; then
+    FOLDER_ID="TBD" # folder: firecloud.org/prod # todo: to be created as part of https://broadworkbench.atlassian.net/browse/CA-1194 after completion of https://broadworkbench.atlassian.net/browse/DDO-1089
+  else
+    echo "ENV was not valid."
+    echo "${VALID_ENVS}"
+    exit 1
+  fi
+fi
+
+RAWLS_SA=$(docker run -e VAULT_TOKEN="$(cat ~/.vault-token)" -it broadinstitute/dsde-toolbox:dev vault read -field="client_email" secret/dsde/firecloud/${vault_env}/rawls/rawls-account.json)
+CROMWELL_SA=$(docker run -e VAULT_TOKEN="$(cat ~/.vault-token)" -it broadinstitute/dsde-toolbox:dev vault read -field="client_email" secret/dsde/firecloud/${vault_env}/cromwell/cromwell-account.json)
+gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${RAWLS_SA}" --role=roles/editor
+gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${CROMWELL_SA}" --role=roles/editor
+
+
+# prod only needs the terra-billing group
+if [[ "${ENV}" == "prod" ]]; then
+  gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:terra-billing@firecloud.org --role=roles/owner
+else # for non-prod envs, add terra-billing as well as project owner groups
+  gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:terra-billing@test.firecloud.org --role=roles/owner
+  gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:firecloud-project-owners@test.firecloud.org --role=roles/owner
+
+  # for the QA env, add quality.firecloud.org users as well
+  if [[ "${ENV}" == "fiab-qa" ]]; then
+    gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:terra-billing@quality.firecloud.org --role=roles/owner
+    gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=group:firecloud-project-owners@quality.firecloud.org --role=roles/owner
+  fi
+fi
---
+++ scripts/project-setup-scripts/create-custom-iam-roles-for-projects.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#create custom roles in each environment's google project
+
+set -euxo pipefail
+
+# todo: convert this to Terraform as part of https://broadworkbench.atlassian.net/browse/CA-1195
+
+ORGANIZATION_ID=$1
+
+if [[ -z "${ORGANIZATION_ID}" ]]
+then
+  echo "Usage: ./create-custom-iam-roles-for-projects.sh <ORGANIZATION_ID>"
+  echo "Organization IDs:
+        test.firecloud.org - 400176686919
+        quality.firecloud.org - 206744735509
+        firecloud.org - 386193000800
+       "
+  exit 1
+else
+  gcloud iam roles create terra-workspace-can-compute --organization=${ORGANIZATION_ID} --file=terra-workspace-can-compute.yaml
+  gcloud iam roles create terra-billing-project-owner --organization=${ORGANIZATION_ID} --file=terra-billing-project-owner.yaml
+fi
